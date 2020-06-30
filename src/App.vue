18c9bceb703681122613f340532f9327e9d32c81
k_asfian <mk.asfian@gmail.com>
2020-06-30 21:05:08 +0700

    added Header component to main App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <Header />
     <Todos v-bind:todosData="todosData" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 
 export default {
   name: "App",
   components: {
+    Header,
     Todos,
   },
   data() {
