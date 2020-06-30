ac19bf511bad98dd1c9935f590879235aeb2a297
k_asfian <mk.asfian@gmail.com>
2020-06-30 10:21:27 +0700

    added Todos component & pass data from App.vue

---
+++ src/components/Todos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div v-for="todo in todosData" :key="todo.id">
+      <h3>{{ todo.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  props: ["todosData"],
+};
+</script>
+
+<style scoped></style>
